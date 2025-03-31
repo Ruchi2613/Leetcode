7e5054a8696638c23f0ee7d4e3dd97217014e142
Ruchi2613 <ruchipandey2606@gmail.com>
2025-03-31 10:47:42 -0700

    stack problems

--- 2025/March/27/stack.py
+++ 2025/March/27/stack.py
@@ -70,18 +70,20 @@ i ->    0  1  2  3          <- starts with n-1
 
 
 '''
-Next smallest to right
+# Next smallest to right
+nums = [6,2,5,4,5,1,6]
+stack = []
+res = [-1]*len(nums)
+for i in range(len(nums) - 1, -1, -1):
 
-    for i in range(len(nums) - 1, -1, -1):
+    while stack and stack[-1] >= nums[i]:
+        stack.pop()
 
-        while stack and stack[-1] >= nums[i]:
-            stack.pop()
+    if stack:
+        res[i] = stack[-1]
 
-        if stack:
-            res[i] = stack[-1]
-
-        stack.append(nums[i])
-    return res
+    stack.append(nums[i])
+print("Next smallest to right:",res)
 '''
 cle = nextElement()
 print(cle.findingElement(nums=[1, 3, 2, 4]))
@@ -111,10 +113,13 @@ class findGreaterElementLeft:
 
             stack.append(nums[i])
         return f"Next Greater Element to Left: {res}"
+
+
 cl_obj = findGreaterElementLeft()
 print(cl_obj.findelement(nums=[1, 3, 2, 4]))
 
 '''Next Smaller Element to Left'''
+
 
 class findSmallerElementLeft:
     def findelement1(self, nums: List[int]) -> List[int]:
@@ -136,26 +141,70 @@ class findSmallerElementLeft:
             stack.append(nums[i])
         return f"Next Smaller Element to Left: {res}"
 
+
 cl_obj = findSmallerElementLeft()
 print(cl_obj.findelement1(nums=[1, 3, 2, 4]))
 
 
 class Parenthesis:
-    def onlyoneparenthesis(self,s:str)->bool:
+    def onlyoneparenthesis(self, s: str) -> bool:
 
         count = 0
         for chr in s:
             if chr == '(':
-                count+=1
+                count += 1
             elif chr == ')':
-                count-=1
+                count -= 1
         if count < 0:
             return f"parenthesis_check: False"
 
-        return f"parenthesis_check: {count==0}"
+        return f"parenthesis_check: {count == 0}"
+
+
 obj = Parenthesis()
 print(obj.onlyoneparenthesis(s=')(())'))
 
+'''Leetcode: Online Stock Span'''
 
 
+# Soln: 1
+class onlineStockSpan:
+    def stockspan(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
 
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i - 1, -1, -1):
+                if nums[i] < nums[j]:
+                    res[i] = i - j
+                    break
+        return res
+
+
+obj_st = onlineStockSpan()
+print(obj_st.stockspan(nums=[100, 80, 60, 70, 60, 75, 85]))
+
+
+# soln: 2 (optimized)
+
+class onlineStockSpan:
+
+    def __init__(self):
+        self.stack = []
+
+    def stockspan(self, nums: List[int]) -> List[int]:
+        res = [1]*len(nums)
+
+        for i in range(len(nums)):
+            while self.stack and self.stack[-1][0] <= nums[i]:
+                self.stack.pop()
+
+            if self.stack:
+                res[i] = i - self.stack[-1][1]
+
+            self.stack.append((nums[i],i))
+
+        return res
+
+
+obj_st = onlineStockSpan()
+print(obj_st.stockspan(nums=[100, 80, 60, 70, 60, 75, 85]))
