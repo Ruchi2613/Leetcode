1ac658bce066831be9164023e5bce50dbe0391b6
Ruchi2613 <ruchipandey2606@gmail.com>
2025-05-23 11:29:19 -0700

    501 leetcode

---
+++ 501_Find_mode_in_Binary_tree.py
@@ -0,0 +1,61 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import Optional, List
+
+
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        d = {}
+
+        def dfs(node):
+            if node is None:
+                return
+
+            if node.val in d:
+                d[node.val]+=1
+            else:
+                d[node.val]=1
+
+            dfs(node.left)
+            dfs(node.right)
+
+            return
+
+        dfs(root)
+
+        print(d)
+
+        k_node_list = list()
+        maxi_val= max(d.values())
+        # print(maxi_val)
+
+        for k, val in d.items():
+            if val == maxi_val:
+                k_node_list.append(k)
+        return k_node_list
+
+        # editorial 1 soln:
+        #
+        # def dfs(node, counter):
+        #
+        #     if node is None:
+        #         return
+        #
+        #     counter[node.val] += 1
+        #     dfs(node.left, counter)
+        #     dfs(node.right, counter)
+        #
+        # counter = defaultdict(int)
+        # dfs(root, counter)
+        # max_freq = max(counter.values())
+        #
+        # ans = []
+        # for key in counter:
+        #     if counter[key] == max_freq:
+        #         ans.append(key)
+        # return ans
+
