13e713e4a8a89e28d6301a0d7dfae45dabe429db
Ruchi2613 <ruchipandey2606@gmail.com>
2025-05-23 15:22:04 -0700

    501 and 530 leetcode

--- 501_Find_mode_in_Binary_tree.py
+++ 501_Find_mode_in_Binary_tree.py
@@ -59,3 +59,50 @@ class Solution:
         #         ans.append(key)
         # return ans
 
+        '''editorial soln 2: Deque(BFS)'''
+
+        # counter = defaultdict(int)
+        # queue = deque([root])
+        #
+        # while queue:
+        #     node = queue.popleft()
+        #     counter[node.val] += 1
+        #
+        #     if node.left:
+        #         queue.append(node.left)
+        #     if node.right:
+        #         queue.append(node.right)
+        #
+        # max_freq = max(counter.values())
+        #
+        # ans = []
+        # for key in counter:
+        #     if counter[key] == max_freq:
+        #         ans.append(key)
+        #
+        # return ans
+
+        '''editorial soln 3'''
+
+        # counter = defaultdict(int)
+        # stack = [root]
+        #
+        # while stack:
+        #     node = stack.pop()
+        #     counter[node.val] += 1
+        #
+        #     if node.left:
+        #         stack.append(node.left)
+        #     if node.right:
+        #         stack.append(node.right)
+        #
+        # max_freq = max(counter.values())
+        #
+        # ans = []
+        # for key in counter:
+        #     if counter[key] == max_freq:
+        #         ans.append(key)
+        #
+        # return ans
+
+
