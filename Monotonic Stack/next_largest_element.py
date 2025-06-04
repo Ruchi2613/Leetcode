5b31362a8cc188f85ac5826a78bde8dfd2de0830
Ruchi2613 <ruchipandey2606@gmail.com>
2025-06-04 00:06:36 -0700

    Monotonic stack prob 1: next largest element to right(2nd Solu)

--- Monotonic Stack/next_largest_element.py
+++ Monotonic Stack/next_largest_element.py
@@ -1,18 +1,34 @@
-from typing import List
+from typing import List, Any
 
 
 class next_largest_element_to_right:
-    def next_largest_element(self,nums: List[int]) -> int:
+    def next_largest_element(self,nums: List[int]) -> list[Any]:
 
-        ans = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] < nums[j]:
-                    ans.append(nums[i])
-                    break
-        ans.append(-1)
-        return ans
+        # ans = []
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] < nums[j]:
+        #             ans.append(nums[i])
+        #             break
+        # ans.append(-1)
+        # return ans
 
+        """if ever j is depended on i then it is a stack problem"""
+
+        stack = []
+        res = []
+        i = len(nums) - 1
+        while i >= 0:
+            if not stack:
+                res.append(-1)
+            elif stack[-1] > nums[i]:
+                res.append(stack[-1])
+            elif stack[-1] <= nums[i]:
+                stack.pop()
+                continue
+            stack.append(nums[i])
+            i -= 1
+        return res[::-1]
 
 cl = next_largest_element_to_right()
 print(cl.next_largest_element(nums=[1, 3, 0, 0, 1, 2, 4]))
