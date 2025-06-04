e5ccb1fd1f14025dafca27c6f6b151010543fec1
Ruchi2613 <ruchipandey2606@gmail.com>
2025-06-04 11:00:13 -0700

    Monotonic stack prob 1: next largest element to right

--- Monotonic Stack/next_largest_element.py
+++ Monotonic Stack/next_largest_element.py
@@ -15,20 +15,35 @@ class next_largest_element_to_right:
 
         """if ever j is depended on i then it is a stack problem"""
 
+        # stack = []
+        # res = []
+        # i = len(nums) - 1
+        # while i >= 0:
+        #     if not stack:
+        #         res.append(-1)
+        #     elif stack[-1] > nums[i]:
+        #         res.append(stack[-1])
+        #     elif stack[-1] <= nums[i]:
+        #         stack.pop()
+        #         continue
+        #     stack.append(nums[i])
+        #     i -= 1
+        # return res[::-1]
+
         stack = []
         res = []
-        i = len(nums) - 1
-        while i >= 0:
+        for i in range(len(nums)-1, -1, -1):
             if not stack:
                 res.append(-1)
-            elif stack[-1] > nums[i]:
+            elif nums[i] < stack[-1]:
                 res.append(stack[-1])
-            elif stack[-1] <= nums[i]:
-                stack.pop()
-                continue
+            elif nums[i] >= stack[-1]:
+                while stack and nums[i] >= stack[-1]:
+                    stack.pop()
+                res.append(stack[-1])
             stack.append(nums[i])
-            i -= 1
+
         return res[::-1]
 
 cl = next_largest_element_to_right()
-print(cl.next_largest_element(nums=[1, 3, 0, 0, 1, 2, 4]))
+print(cl.next_largest_element(nums=[1, 3, 0, 0, 1, 2, 4])) # [3, 4, 1, 1, 2, 4, -1]
