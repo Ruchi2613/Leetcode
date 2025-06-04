9fc953fd4b1ae2c6731f9e32e95922c298eacf58
Ruchi2613 <ruchipandey2606@gmail.com>
2025-06-04 14:19:15 -0700

    Monotonic stack prob 1: next largest element to left

---
+++ Monotonic Stack/next_largest_element_to_left.py
@@ -0,0 +1,26 @@
+from typing import List, Any
+
+
+class next_largest_element_to_left:
+    def next_largest_element(self,nums: List[int]) -> list[Any]:
+
+        stack = []
+        res = []
+
+        for num in nums:
+            if not stack:
+                res.append(-1)
+            elif num < stack[-1]:
+                res.append(stack[-1])
+            elif num >= stack[-1]:
+                while stack and num >= stack[-1]:
+                    stack.pop()
+                res.append(-1)
+            stack.append(num)
+
+        return res
+
+
+
+cl = next_largest_element_to_left()
+print(cl.next_largest_element(nums=[1,3,2,4]))
