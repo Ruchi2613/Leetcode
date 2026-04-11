26c2bdea2de54256c6cd9d3096d8e1f52a394a14
Ruchi2613 <ruchipandey2606@gmail.com>
2026-04-11 15:44:16 -0700

    solution of Maximum Product of Three Numbers

--- Array/628. Maximum Product of Three Numbers.py
+++ Array/628. Maximum Product of Three Numbers.py
@@ -25,3 +25,13 @@ Constraints:
 
 3 <= nums.length <= 104
 -1000 <= nums[i] <= 1000'''
+
+class Solution(object):
+    def maximumProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        nums.sort()
+        return max(nums[0] * nums[1] * nums[-1], nums[-1] * nums[-2] * nums[-3])
