25faa40d78e0401566e6163dee702d4626637937
Ruchi2613 <ruchipandey2606@gmail.com>
2026-04-13 11:22:46 -0700

    3637. Trionic Array I

---
+++ Array/3637. Trionic Array I.py
@@ -0,0 +1,72 @@
+'''3637. Trionic Array I
+Solved
+Easy
+Topics
+conpanies icon
+Companies
+Hint
+You are given an integer array nums of length n.
+
+An array is trionic if there exist indices 0 < p < q < n − 1 such that:
+
+nums[0...p] is strictly increasing,
+nums[p...q] is strictly decreasing,
+nums[q...n − 1] is strictly increasing.
+Return true if nums is trionic, otherwise return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,4,2,6]
+
+Output: true
+
+Explanation:
+
+Pick p = 2, q = 4:
+
+nums[0...2] = [1, 3, 5] is strictly increasing (1 < 3 < 5).
+nums[2...4] = [5, 4, 2] is strictly decreasing (5 > 4 > 2).
+nums[4...5] = [2, 6] is strictly increasing (2 < 6).
+Example 2:
+
+Input: nums = [2,1,3]
+
+Output: false
+
+Explanation:
+
+There is no way to pick p and q to form the required three segments.
+
+ 
+
+Constraints:
+
+3 <= n <= 100
+-1000 <= nums[i] <= 1000'''
+
+class Solution(object):
+    def isTrionic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        first_inc_level = False
+        sec_dec_level = False 
+        third_level = False
+
+        for i in range(len(nums)-1):
+            if nums[i] < nums[i+1] and sec_dec_level == False and third_level == False:
+                first_inc_level = True
+            elif nums[i] > nums[i+1] and first_inc_level and third_level == False:
+                sec_dec_level = True
+            elif nums[i] < nums[i+1] and first_inc_level and sec_dec_level:
+                third_level = True  
+            else:
+                return False
+
+        return first_inc_level and sec_dec_level and third_level
+
+        
