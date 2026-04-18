9deb47a4a45f232a503746791be7c54caa3dca30
Ruchi2613 <ruchipandey2606@gmail.com>
2026-04-17 18:56:55 -0700

    solution of assign cookie

--- Array/455. Assign Cookies.py
+++ Array/455. Assign Cookies.py
@@ -31,3 +31,21 @@ Constraints:
 0 <= s.length <= 3 * 104
 1 <= g[i], s[j] <= 231 - 1
 '''
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+
+        count = 0
+        g.sort()
+        s.sort()
+
+        i, j = 0,0
+
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                count+=1
+                i+=1
+            j+=1
+
+        return count
+
