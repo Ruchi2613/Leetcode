a97c100111ce8f95cea9e5065eff12272112901e
Ruchi2613 <ruchipandey2606@gmail.com>
2026-05-05 14:27:35 -0700

    Monotonic stack uisng linkedList

---
+++ LinkedList/medium/1019. Next Greater Node In Linked List.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+
+        temp_list = []
+
+        temp = head
+
+        while temp is not None:
+            temp_list.append(temp.val)
+            temp = temp.next
+
+
+        res = [0]* len(temp_list)
+
+        stack = []
+
+        for i in range(len(temp_list)):
+
+            while stack and temp_list[stack[-1]] < temp_list[i]:
+                # res[stack.pop()] = temp_list[i]
+                smaller_index = stack.pop()
+                res[smaller_index] = temp_list[i]
+            stack.append(i)
+
+        return res
