b8856fe148ea7c7ddf38739ca4a956765a9ad36a
Ruchi2613 <ruchipandey2606@gmail.com>
2026-05-11 12:01:00 -0700

    sol

---
+++ Dictionary/3663. Find The Least Frequent Digit.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getLeastFrequentDigit(self, n: int) -> int:
+        
+        d = {}
+
+        for x in str(n):
+            if int(x) in d:
+                d[int(x)]+=1
+            else:
+                d[int(x)] = 1
+
+        mini = sys.maxsize
+        mini_el = 0
+        
+        for ele, freq in d.items():
+            if mini > freq:
+                mini = freq
+                mini_el = ele
+            elif mini == freq:
+                mini_el = min(mini_el, ele)
+
+        return mini_el
