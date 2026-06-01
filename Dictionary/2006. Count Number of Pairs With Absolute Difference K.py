733b12533d88f6ba9b8ef730483545e151c93809
Ruchi2613 <ruchipandey2606@gmail.com>
2026-05-31 20:04:34 -0700

    2006. Count Number of Pairs With Absolute Difference K

--- Dictionary/2006. Count Number of Pairs With Absolute Difference K.py
+++ Dictionary/2006. Count Number of Pairs With Absolute Difference K.py
@@ -54,3 +54,16 @@ class Solution:
                     count+=1
         
         return count
+    
+        # Best Solution:
+        nums_dict, count = {}, 0
+        for num in nums:
+            if num not in nums_dict:
+                nums_dict[num] = 1
+            else:
+                nums_dict[num] += 1
+
+        for num in nums:
+            if num + k in nums_dict:
+                count += nums_dict[num + k]
+        return count
