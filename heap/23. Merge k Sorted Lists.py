69a8248140407eac514ee954cca3605c91d91a58
Ruchi2613 <ruchipandey2606@gmail.com>
2026-06-15 21:47:45 -0700

    Merge k Sorted Lists

---
+++ heap/23. Merge k Sorted Lists.py
@@ -0,0 +1,98 @@
+'''23. Merge k Sorted Lists
+Solved
+Hard
+Topics
+conpanies icon
+Companies
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted linked list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+ 
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+3,040,987/5.1M
+Acceptance Rate
+59.8%'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import heapq
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        heap = []
+        dummy = ListNode(0)
+        temp = dummy
+
+        for i in range(len(lists)):
+            node = lists[i]
+
+            if node:
+                heapq.heappush(heap,(node.val,i,node))
+
+        while len(heap)>0:
+            val,ind,node = heapq.heappop(heap)
+            temp.next = ListNode(val)
+            temp = temp.next
+            if node.next is not None:
+                node = node.next
+                heapq.heappush(heap,(node.val,ind,node))
+        
+        return dummy.next
+
+        # node = []
+        # dummy = ListNode(0)
+        # temp = dummy
+
+        # for x in lists:
+        #     while x is not None:
+        #         node.append(x.val)
+        #         x = x.next
+
+        # for y in sorted(node):
+        #     temp.next = ListNode(y)
+        #     temp = temp.next
+        
+        # return dummy.next
+
